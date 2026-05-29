b8db7f4222de7bc43b06f31b4ef9a41326d28c5b
Rupam  Das <rupamdas3941@gmail.com>
2026-05-29 19:29:29 +0530

    Update settings.py

--- hms/hms/settings.py
+++ hms/hms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t&=$1m(@+_bo4&j2!g3psgq0aso#$c1q)a7z=)sw86vuyhs51s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,4 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
